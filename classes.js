5977548c6efe64b04391ced948dab47fe1504a82
pagecow <pagecow@gmail.com>
2019-09-19 14:27:00 -0600

    started the project

--- classes.js
+++ classes.js
@@ -29,7 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name,
+    this.last_name = last_name,
+    this.email = email,
+    this.age = age
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +59,29 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age),
+    this.reports = []
+  }
+
+  hire(employee){
+    this.reports.push(employee)
+  }
+
+  fire(index){
+    for(var prop in this.reports){
+      if(prop === index){
+        delete this.reports[prop]
+      }
+    }
+  }
+}
+
+// ----------------------- help
+
+
+
 
 
 ////////// PROBLEM 3 //////////
